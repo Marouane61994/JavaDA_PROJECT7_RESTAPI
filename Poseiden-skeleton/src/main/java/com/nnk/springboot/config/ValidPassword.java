fc053d507d8f8f190d223b61a1cb00aed9d233fa
Marouane61994 <marouanneghanem@hotmail.fr>
2025-06-19 19:06:41 +0200

    Implemented password format validation

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/ValidPassword.java
@@ -0,0 +1,18 @@
+package com.nnk.springboot.config;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+    String message() default "Le mot de passe doit contenir au moins 8 caractères, une majuscule, un chiffre et un symbole.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
+
